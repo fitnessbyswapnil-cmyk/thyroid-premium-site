cd121cbd93533cdb34b7f84dcc724496b2caec56
Swapnil Umbarkar <swapnil@Swapnils-MacBook-Air.local>
2026-05-17 12:35:24 +0530

    mobile optimization

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,33 @@
 import Hero from "@/app/components/Hero";
 import ThyroidStrategySession from "@/app/components/ThyroidStrategySession";
 import SocialProof from "@/app/components/SocialProof";
+import ResultsSection from "@/app/components/ResultsSection";
+import VideoTestimonial from "@/app/components/VideoTestimonial";
+import ProblemSection from "@/app/components/ProblemSection";
+import WhatsappProofSection from "@/app/components/WhatsappProofSection";
+import AuthoritySection from "@/app/components/AuthoritySection";
+import FrameworkSection from "@/app/components/FrameworkSection";
+import MoreThanFatLossSection from "@/app/components/MoreThanFatLossSection";
+import FAQSection from "@/app/components/FAQSection";
+import FinalCTASection from "@/app/components/FinalCTASection";
+import StickyBookingBar from "@/app/components/StickyBookingBar";
 
 export default function Home() {
   return (
     <main>
       <Hero />
-
-      {/* Premium consultation process */}
       <ThyroidStrategySession />
-
       <SocialProof />
+      <ResultsSection />
+      <VideoTestimonial />
+      <ProblemSection />
+      <WhatsappProofSection />
+      <AuthoritySection />
+      <FrameworkSection />
+      <MoreThanFatLossSection />
+      <FAQSection />
+      <FinalCTASection />
+      <StickyBookingBar />
     </main>
   );
 }
